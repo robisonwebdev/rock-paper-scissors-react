37904c4d6a6e3515698c03835d5de42b3c510ef1
robisonwebdev <david@robisonweb.dev>
2021-09-24 09:23:14 -0600

    Destructured computerScore and playerScore. Passed computerScore and playerScore to Scoreboard component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Scoreboard from './Scoreboard';
 
-const Header = () => {
+const Header = ({ computerScore, playerScore }) => {
   return (
     <header>
-      <Scoreboard />
+      <Scoreboard
+        computerScore={computerScore}
+        playerScore={playerScore}
+      />
     </header>
   );
 }
