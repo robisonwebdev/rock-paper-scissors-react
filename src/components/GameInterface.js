312f6acdee16668bc53c2508338fd1e31035b9f2
robisonwebdev <david@robisonweb.dev>
2021-09-25 09:29:21 -0600

    Added icon props to all GamePiece

--- src/components/GameInterface.js
+++ src/components/GameInterface.js
@@ -5,9 +5,9 @@ const GameInterface = ({ competitor }) => {
   return (
     <div className='gameInterface'>
       <h2>{competitor}</h2>
-      <GamePiece />
-      <GamePiece />
-      <GamePiece />
+      <GamePiece icon='las la-hand-rock' />
+      <GamePiece icon='las la-hand-paper' />
+      <GamePiece icon='las la-hand-scissors' />
     </div>
   );
 }
