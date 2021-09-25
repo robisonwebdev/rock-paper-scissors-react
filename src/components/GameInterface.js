eb0cde227698929b4434f841c37ee0d8f2d223e9
robisonwebdev <david@robisonweb.dev>
2021-09-25 09:18:24 -0600

    Imported GamePiece.js and added it three times to .gameInterface.

--- src/components/GameInterface.js
+++ src/components/GameInterface.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import GamePiece from './GamePiece';
 
 const GameInterface = ({ competitor }) => {
   return (
     <div className='gameInterface'>
       <h2>{competitor}</h2>
+      <GamePiece />
+      <GamePiece />
+      <GamePiece />
     </div>
   );
 }
