d89e980a50933460c99bd3d2ec91c188d3b72f3b
robisonwebdev <david@robisonweb.dev>
2021-09-26 06:48:12 -0600

    Imported GameStatus.js Added GameStatus component to .gameInfo

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import GameForm from './GameForm';
+import GameStatus from './GameStatus';
 import '../styles/GameInfo.css';
 
 const GameInfo = () => {
     return (
         <div className='gameInfo'>
             <GameForm />
+            <GameStatus />
         </div>
     );
 }
