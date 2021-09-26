487ad8e81867cf9b9aae8b5df9a74a20322033dc
robisonwebdev <david@robisonweb.dev>
2021-09-26 06:25:09 -0600

    Created handleSubmit function and added preventDefault. Setup onSubmit on form and passed it handleSubmit.

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const GameForm = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type='text' name='name' placeholder='Enter your name' />
             <input type='submit' value='Submit' />
         </form>
