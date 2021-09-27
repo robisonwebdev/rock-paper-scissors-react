2bd7f5b54d7dfc67f54afa50149df781734b6a9b
robisonwebdev <david@robisonweb.dev>
2021-09-27 08:28:24 -0600

    Imported useState. Setup useState for showResetBtn, and set to initial value of false.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Gameboard from './Gameboard';
 import Button from './Button';
 import '../styles/Main.css';
 
 const Main = () => {
+  const [showResetBtn, setShowResetBtn] = useState(false);
+
   return (
     <main>
       <Gameboard />
