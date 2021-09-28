1e7ab2a5417d4065362efe52f78dfb11f3f759a4
robisonwebdev <david@robisonweb.dev>
2021-09-28 09:20:11 -0600

    Destructured className and added className to button.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/Button.css';
 
-const Button = ({ text, type }) => {
+const Button = ({ className, text, type }) => {
   return (
-    <button type={type}>{text}</button>
+    <button className={className} type={type}>{text}</button>
   );
 }
 
