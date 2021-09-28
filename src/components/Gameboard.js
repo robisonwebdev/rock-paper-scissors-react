4b67659a132edfba82c25e13b020b0ffccfb4fd6
robisonwebdev <david@robisonweb.dev>
2021-09-28 09:38:15 -0600

    Destructured props and passed with to GameInfo component.

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -3,12 +3,16 @@ import GameInterface from './GameInterface';
 import GameInfo from './GameInfo';
 import '../styles/Gameboard.css';
 
-const Gameboard = ({ setPlayerName }) => {
+const Gameboard = ({ setPlayerName, setShowGameForm, setShowGameStatus, showGameForm, showGameStatus }) => {
   return (
     <div className='gameboard'>
       <GameInterface competitor='Player' />
       <GameInfo
         setPlayerName={setPlayerName}
+        setShowGameForm={setShowGameForm}
+        setShowGameStatus={setShowGameStatus}
+        showGameForm={showGameForm}
+        showGameStatus={showGameStatus}
       />
       <GameInterface competitor='Computer' />
     </div>
