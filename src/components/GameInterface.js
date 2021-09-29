c9e2da18251288aea71e42c21dce4122e5e1be80
robisonwebdev <david@robisonweb.dev>
2021-09-29 08:04:16 -0600

    Destructured gamePieceClick and passed it to onClick prop for GamePiece component.

--- src/components/GameInterface.js
+++ src/components/GameInterface.js
@@ -2,13 +2,13 @@ import React from 'react';
 import GamePiece from './GamePiece';
 import '../styles/GameInterface.css';
 
-const GameInterface = ({ className, competitor }) => {
+const GameInterface = ({ className, competitor, gamePieceClick }) => {
   return (
     <div className={`gameInterface ${className}`}>
       <h2>{competitor || className}</h2>
-      <GamePiece icon='las la-hand-rock' />
-      <GamePiece icon='las la-hand-paper' />
-      <GamePiece icon='las la-hand-scissors' />
+      <GamePiece icon='las la-hand-rock' onClick={gamePieceClick} />
+      <GamePiece icon='las la-hand-paper' onClick={gamePieceClick} />
+      <GamePiece icon='las la-hand-scissors' onClick={gamePieceClick} />
     </div>
   );
 }
