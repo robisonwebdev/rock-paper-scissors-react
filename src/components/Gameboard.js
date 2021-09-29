9faea56bacba3e7d34db632436d13ca5da09c0ee
robisonwebdev <david@robisonweb.dev>
2021-09-29 08:02:34 -0600

    Destructured gamePieceClick and passed it to GameInterface component.

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -3,12 +3,13 @@ import GameInterface from './GameInterface';
 import GameInfo from './GameInfo';
 import '../styles/Gameboard.css';
 
-const Gameboard = ({ message, playerName, setPlayerName, setShowResetBtn, setShowGameForm, setShowGameStatus, showGameForm, showGameStatus }) => {
+const Gameboard = ({ gamePieceClick, message, playerName, setPlayerName, setShowResetBtn, setShowGameForm, setShowGameStatus, showGameForm, showGameStatus }) => {
   return (
     <div className='gameboard'>
       <GameInterface
         className='Player'
         competitor={playerName}
+        gamePieceClick={gamePieceClick}
       />
       <GameInfo
         message={message}
