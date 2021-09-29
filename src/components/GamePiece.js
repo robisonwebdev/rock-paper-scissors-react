e7c7a2d9b35afab2a21cc6c7d4196bd150533e72
robisonwebdev <david@robisonweb.dev>
2021-09-29 08:00:17 -0600

    Destrucutred onClick and added it to onClick event

--- src/components/GamePiece.js
+++ src/components/GamePiece.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/GamePiece.css';
 
-const GamePiece = ({ icon }) => {
+const GamePiece = ({ icon, onClick }) => {
     return (
-        <i className={`gamePiece ${icon}`}></i>
+        <i className={`gamePiece ${icon}`} onClick={onClick}></i>
     );
 }
 
