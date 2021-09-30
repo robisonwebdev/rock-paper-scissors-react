0209f37fdd7707709a78cfbdcef4e28b34be5d8b
robisonwebdev <david@robisonweb.dev>
2021-09-30 10:37:20 -0600

    Passed data_value to GamePiece

--- src/components/GameInterface.js
+++ src/components/GameInterface.js
@@ -6,9 +6,9 @@ const GameInterface = ({ className, competitor, gamePieceClick }) => {
   return (
     <div className={`gameInterface ${className}`}>
       <h2>{competitor || className}</h2>
-      <GamePiece icon='las la-hand-rock' onClick={gamePieceClick} />
-      <GamePiece icon='las la-hand-paper' onClick={gamePieceClick} />
-      <GamePiece icon='las la-hand-scissors' onClick={gamePieceClick} />
+      <GamePiece data_value='rock' icon='las la-hand-rock' onClick={gamePieceClick} />
+      <GamePiece data_value='paper' icon='las la-hand-paper' onClick={gamePieceClick} />
+      <GamePiece data_value='scissors' icon='las la-hand-scissors' onClick={gamePieceClick} />
     </div>
   );
 }
