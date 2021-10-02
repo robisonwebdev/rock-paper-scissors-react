64663a78db68b1aa82fc08f24b780c6fc72aaa06
robisonwebdev <david@robisonweb.dev>
2021-10-02 10:14:42 -0600

    Destrucuted className. Set className in className.

--- src/components/GamePiece.js
+++ src/components/GamePiece.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../styles/GamePiece.css';
 
-const GamePiece = ({ data_value, icon, onClick }) => {
+const GamePiece = ({ data_value, className, icon, onClick }) => {
     return (
         <i
-            className={`gamePiece ${icon}`}
+            className={`gamePiece ${icon} ${className}`}
             data_value={data_value}
             onClick={onClick}
         >
