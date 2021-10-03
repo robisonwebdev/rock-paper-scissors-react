2c3f2b1c219fa1f72ca5cf4408d8b2d36522e505
robisonwebdev <david@robisonweb.dev>
2021-10-03 08:52:10 -0600

    Moved GamePiece components into a new div with className of gamePieces

--- src/components/GameInterface.js
+++ src/components/GameInterface.js
@@ -12,24 +12,26 @@ const GameInterface = ({ className, competitor, gamePieceClick, gameStart }) => 
   return (
     <div className={`gameInterface ${className}`}>
       <h2>{competitor || className}</h2>
-      <GamePiece
-        data_value='rock'
-        className={iconHover}
-        icon='las la-hand-rock'
-        onClick={gamePieceClick}
-      />
-      <GamePiece
-        data_value='paper'
-        className={iconHover}
-        icon='las la-hand-paper'
-        onClick={gamePieceClick}
-      />
-      <GamePiece
-        data_value='scissors'
-        className={iconHover}
-        icon='las la-hand-scissors'
-        onClick={gamePieceClick}
-      />
+      <div className='gamePieces'>
+        <GamePiece
+          data_value='rock'
+          className={iconHover}
+          icon='las la-hand-rock'
+          onClick={gamePieceClick}
+        />
+        <GamePiece
+          data_value='paper'
+          className={iconHover}
+          icon='las la-hand-paper'
+          onClick={gamePieceClick}
+        />
+        <GamePiece
+          data_value='scissors'
+          className={iconHover}
+          icon='las la-hand-scissors'
+          onClick={gamePieceClick}
+        />
+      </div>
     </div>
   );
 }
